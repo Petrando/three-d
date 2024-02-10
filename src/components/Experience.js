fdf1cc11822f04fb75bd6ab5f213e3e06ffc0c5d
Petrando <petrandorichard@gmail.com>
2024-02-10 21:43:06 +0800

    can display my 3d avatar

---
+++ src/components/Experience.js
@@ -0,0 +1,16 @@
+"use client"
+import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar"
+
+export const Experience = () => {
+    //
+    return (
+        <>
+            <OrbitControls />
+            <group position-y={-1}>
+                <Avatar />
+            </group>
+            <ambientLight intensity={1} />
+        </>
+    );
+};
